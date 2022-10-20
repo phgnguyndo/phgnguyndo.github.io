43d63030102f51378db916d6ff84d7f0fadc0754
phgnguyndo <nguyenphuongdo22@gmail.com>
2022-10-20 23:04:17 +0700

    Updates

---
+++ precache-manifest.06714929fda54c4c451dfb6cda2ce009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9259f2a1763e3f4e275c",
+    "url": "/web1/static/css/main.79a5bf20.chunk.css"
+  },
+  {
+    "revision": "9259f2a1763e3f4e275c",
+    "url": "/web1/static/js/main.9259f2a1.chunk.js"
+  },
+  {
+    "revision": "bfa36157bcc75fb8ea07",
+    "url": "/web1/static/js/1.bfa36157.chunk.js"
+  },
+  {
+    "revision": "7587d2c9267632307ee0",
+    "url": "/web1/static/js/2.7587d2c9.chunk.js"
+  },
+  {
+    "revision": "5107eab7a76efbf0aaa2",
+    "url": "/web1/static/js/runtime~main.5107eab7.js"
+  },
+  {
+    "revision": "e6b6c1f941f4d70560e725b79459177d",
+    "url": "/web1/index.html"
+  }
+];
